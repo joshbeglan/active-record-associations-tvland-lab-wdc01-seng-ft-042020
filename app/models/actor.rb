06945e9ad1d3dcd02c1dbb67f271679d9f6158f8
mertoz41 <mertozkaynak@windowslive.com>
2020-05-01 15:57:00 -0400

    first commit

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
 class Actor < ActiveRecord::Base
+
+    has_many :characters 
+    has_many :shows, through: :characters
+
+    def full_name
+       return "#{self.first_name} #{self.last_name}"
+    end 
+
+    def list_roles
+     characters = Character.all.select {|character| character.actor_id == self.id}
+     name = characters.select {|character| character.name}
+     shows = characters.map {|char| char.show_id}
+     show = shows.select {|show| show.name}
+     puts "#{name} - #{show}"
+   
+
+    end 
+    
+        
   
 end
